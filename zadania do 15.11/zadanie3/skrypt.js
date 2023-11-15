33dab3c77b8f4bbb081951b99dbc4d1b41bdc32a
ZelazneKrzeslo <89976180+ZelazneKrzeslo@users.noreply.github.com>
2023-11-15 22:58:35 +0100

    Delete zadania do 15.11 directory

--- zadania do 15.11/zadanie3/skrypt.js
+++
@@ -1,6 +0,0 @@
-const tablica1 = ["Hania", "Ania"];
-const tablica2 = ["Gregor", "Henio"];
-
-const polaczone = tablica1.concat(tablica2).join(", ");
-
-console.log(polaczone);
--- zadania do 15.11/zadanie4/skrypt.js
+++
@@ -1,6 +0,0 @@
-const tab = [10, 11, "ania", 11];
-const f = 11;
-
-const wynik = tab.filter(element => element === f);
-
-console.log(wynik);
